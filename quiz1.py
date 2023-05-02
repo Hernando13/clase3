3a3323f195399e334e62bc7ba6e35db451984a12
Hernando13 <j.hernando@correo.unimet.edu.ve>
2023-05-02 18:59:05 -0400

    uploaded quiz

---
+++ quiz1.py
@@ -0,0 +1,11 @@
+ahorro = input("introduce el monto")
+ahorro = float(ahorro)
+
+a1 = ahorro *0.04
+print (ahorro + a1)
+a2 = ahorro *0.08
+print (ahorro + a2)
+a3 = ahorro *0.12
+print(ahorro + a3)
+
+
